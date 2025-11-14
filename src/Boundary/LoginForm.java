d594e606187b466345b24857cfa7032389b71c8d
mpruner <mpruner@augusta.edu>
2025-11-14 01:38:02 -0500

    gave login window a makeover

--- src/Boundary/LoginForm.java
+++ src/Boundary/LoginForm.java
@@ -1,14 +1,20 @@
 package Boundary;
 
 import Control.StartController;
-
+import javafx.geometry.HPos;
 // import javafx.application.Platform;
 // import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class LoginForm {
@@ -18,38 +24,100 @@ public class LoginForm {
 
     public LoginForm(StartController controller) {
         this.controller = controller;
-        //loginPane = new VBox();
     }
 
     public void show(Stage stage) {
-        VBox loginPane = new VBox(10);
-        loginPane.setPadding(new Insets(20));
+        // grid pane holds the login components (labels, fields, button)
+        GridPane loginPane = new GridPane();
+        loginPane.setPadding(new Insets(0));
+        loginPane.setHgap(10);
+        loginPane.setVgap(10);
 
+        // border
+        loginPane.setStyle(
+            "-fx-border-color: gray;" +
+            "-fx-border-width: 2;" +
+            "-fx-border-radius: 5;" +
+            "-fx-background-color: white;" +
+            "-fx-padding: 10;"
+        );
+
+        // Make grid 3x3
+        // Left column - 20% width
+        ColumnConstraints col1 = new ColumnConstraints();
+        col1.setPercentWidth(28);
+        // Center column - 60% width
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setPercentWidth(44);
+        // Right column - 20% width
+        ColumnConstraints col3 = new ColumnConstraints();
+        col3.setPercentWidth(28);
+
+        // Add constraints to the grid
+        loginPane.getColumnConstraints().addAll(col1, col2, col3);
+
+        // "Login" label
+        Label titleLabel = new Label("Login");
+        titleLabel.setStyle(
+            "-fx-font-size: 16px;" +
+            "-fx-font-weight: bold;"
+        );
+
+        // textfield labels
         Label labelUserName = new Label("Username");
-        TextField userNameField = new TextField();
-        userNameField.setMaxWidth(175);
-
         Label labelPassword = new Label("Password");
+
+        // textfields
+        TextField userNameField = new TextField();
         PasswordField passwordField = new PasswordField();
-        passwordField.setMaxWidth(175);
+        userNameField.setMaxWidth(190);
+        passwordField.setMaxWidth(190);
+
+         // box to hold both labels
+        VBox fieldLabels = new VBox();
+        fieldLabels.setSpacing(25);
+        fieldLabels.getChildren().addAll(labelUserName, labelPassword);
+        fieldLabels.setAlignment(Pos.CENTER_RIGHT);
+
+        // box to hold both input fields
+        VBox inputFields = new VBox();
+        inputFields.setSpacing(20);
+        inputFields.setPadding(new Insets(15, 0, 15, 0));
+        inputFields.getChildren().addAll(userNameField, passwordField);
+        inputFields.setAlignment(Pos.CENTER);
 
         Button loginButton = new Button("Submit");
         loginButton.setOnAction(e -> handleLogin(userNameField.getText(), passwordField.getText()));
 
-        int labelWidth = 60;
-        labelUserName.setPrefWidth(labelWidth);
-        labelPassword.setPrefWidth(labelWidth);
+        // Add items to grid
+        // (col#, row#)
+        loginPane.add(titleLabel, 1, 0);
+        loginPane.add(fieldLabels, 0, 1);
+        loginPane.add(inputFields, 1, 1);
+        loginPane.add(loginButton, 1, 2);
 
-        HBox userPane = new HBox(10);
-        //userPane.setPadding(new Insets(20));
-        userPane.getChildren().addAll(labelUserName, userNameField);
-        HBox passwordPane = new HBox(10);
-        //passwordPane.setPadding(new Insets(20));
-        passwordPane.getChildren().addAll(labelPassword, passwordField);
+        // center title and button
+        GridPane.setHalignment(titleLabel, HPos.CENTER);
+        GridPane.setHalignment(loginButton, HPos.CENTER);
+        
+        // set preferred width/height of grid pane
+        //loginPane.setPrefSize(400, Region.USE_COMPUTED_SIZE);
+        loginPane.setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
 
-        loginPane.getChildren().addAll(userPane, passwordPane, loginButton);
+        // wrapper around grid pane
+        // positions login "window" within main window
+        StackPane loginStack = new StackPane(loginPane);
+        StackPane.setAlignment(loginPane, Pos.CENTER);
 
-        Scene scene = new Scene(loginPane, 400, 300);
+        // get computer display size
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        // compute __% of screen size
+        double width = screenBounds.getWidth() * 0.8;
+        double height = screenBounds.getHeight() * 0.8;
+
+        // the login "scene" for the main window
+        // scene will change after login to either student/teacher menus
+        Scene scene = new Scene(loginStack, width, height);
         stage.setScene(scene);
         stage.setTitle("Login");
         stage.show();
