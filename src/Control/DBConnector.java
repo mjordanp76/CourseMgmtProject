8a34927c817ba9d4432547828f9d7c177ae4fbf2
mpruner <mpruner@augusta.edu>
2025-11-14 01:58:34 -0500

    added initialization feature to StartController & DBConnector

--- src/Control/DBConnector.java
+++ src/Control/DBConnector.java
@@ -1,5 +1,32 @@
 package Control;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnector {
+
+    private Connection conn;
+     private static final String DB_URL = "jdbc:sqlite:CourseMgmtDB.db";
     
+    public DBConnector() {
+    }
+
+    public DBConnector(Connection conn) {
+        this.conn = conn;
+    }
+
+    public void initializeDB() {
+        try {
+            conn = DriverManager.getConnection(DB_URL);
+            System.out.println("Connected to SQLite database!");
+        } catch (SQLException e) {
+            System.err.println("Connection failed: " + e.getMessage());
+            conn = null; // ensure it's null if connection fails
+        }
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
 }
--- src/Control/StartController.java
+++ src/Control/StartController.java
@@ -6,21 +6,21 @@ import javafx.stage.Stage;
 
 public class StartController {
     
-    //private DBConnector;
+    private DBConnector dbConnector;
     private Stage primaryStage;
 
     public StartController() {
-        //dbConnector = new DBConnector();
+        dbConnector = new DBConnector();
     }
 
     public void initiate(Stage primaryStage) {
-        // try {
-        //     dbConnector.initializeDB();
-        // } catch (Exception e) {
-        //     System.err.print("Failed to initialize database: " + e.getMessage() + " Exiting Course Management now.");
-        //     e.printStackTrace();
-        //     return; // stop startup if DB can't be initialized
-        // }
+        try {
+            dbConnector.initializeDB();
+        } catch (Exception e) {
+            System.err.println("Failed to initialize database: " + e.getMessage() + " Exiting Course Management now.");
+            e.printStackTrace();
+            return; // stop startup if DB can't be initialized
+        }
         LoginForm loginForm = new LoginForm(this);
         loginForm.show(primaryStage);
     }
@@ -29,7 +29,7 @@ public class StartController {
      * Used by other controllers to access the DB
      * Avoids having to create DB connections all over the place
      */
-    // public DBConnector getDbConnector() {
-    //     return dbConnector;
-    // }
+    public DBConnector getDbConnector() {
+        return dbConnector;
+    }
 }
